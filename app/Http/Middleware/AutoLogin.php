3fac3d922c86bb85276644671c3a99ea46c1eb07
Lee Jenkins <edgecase123@gmail.com>
2025-05-25 12:53:19 -0400

    Resolves #10 Added middleware to log in default "Test User"

---
+++ app/Http/Middleware/AutoLogin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class AutoLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = User::whereName('Test User')->first();
+
+        if ($user) {
+            Auth::login($user);
+        } else {
+            return \response()->json(['error' => 'User not found. Did you run migrations?']);
+        }
+
+        Log::log('debug', __FUNCTION__ . ': ' . 'Finished auto logging in user', [Auth::user()]);
+
+        return $next($request);
+    }
+}
