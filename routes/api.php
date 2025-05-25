3489e8f2aa090737a66e85513081469ec877f4bc
Lee Jenkins <edgecase123@gmail.com>
2025-05-25 10:56:42 -0400

    Resolves #6. Conifgured cors to allow front-end to connect.

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\Api\v1\TheOneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(static function () {
-   Route::get('/character', [TheOneController::class, 'index']);
+    Route::get('/character', [TheOneController::class, 'index']);
+    Route::get('/sanctum/csrf-cookie', action: array(CsrfCookieController::class, 'show'));
 });
