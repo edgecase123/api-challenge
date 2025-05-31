dfb0c793dbb643586da4e221930a3b0feb8437e4
Lee Jenkins <edgecase123@gmail.com>
2025-05-31 08:47:00 -0400

    general_fixes. Add caching for the main, default TheOneApi query to help reduce calls to the service when running tests

--- app/Http/Controllers/Api/v1/TheOneController.php
+++ app/Http/Controllers/Api/v1/TheOneController.php
@@ -5,23 +5,36 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\AbstractController;
 use App\Services\TheOneApiService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class TheOneController extends AbstractController
 {
+    private const DEFAULT_LIST_KEY = 'default_characters';
+
     public function index(TheOneApiService $oneApiService): JsonResponse
     {
+        Log::log('debug', __FUNCTION__ . ': ' . 'Params: ', [$this->request->all()]);
         $params = $this->validateInput([
             'field' => 'regex:/^[a-z]+$/|min:3|required_with:term|in:name,race,birth,death',
             'term' => 'regex:/^[a-zA-Z0-9\s]+$/|min:3|required_with:field',
             'limit' => 'sometimes|int|min:10|max:100',
         ]);
 
-        $response = $oneApiService->getCharacters(
-            $params['limit'] ?? 100,
-            $params['term'] ?? null,
-            $params['field'] ?? null,
-        );
+        // Use cache for the default show all query to TheOneApi.
+        // Otherwise, retrieve directly
+        if (empty($params['term']) && empty($params['field'])) {
+            $response = Cache::remember(self::DEFAULT_LIST_KEY, now()->addMinutes(10),
+                function () use ($oneApiService) {
+                    return $oneApiService->getCharacters($params['limit'] ?? 100);
+                });
+        } else {
+            $response = $oneApiService->getCharacters(
+                $params['limit'] ?? 100,
+                $params['term'],
+                $params['field'],
+            );
+        }
 
         return response()->json($response ?? ['count' => 0, 'data' => []]);
     }
